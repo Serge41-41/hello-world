31d907328ede5b81734069423e60c089de818360
Serge41-41 <s459114@gmail.com>
2025-08-03 12:34:58 +0300

    Изменения в выводе строки

--- Hello world/src/Main.java
+++ Hello world/src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите имя");
-String name = new Scanner(System.in).nextLine();
-System.out.println(name);
+        String name = new Scanner(System.in).nextLine();
+        System.out.println("Ваше имя " + name);
     }
 }
